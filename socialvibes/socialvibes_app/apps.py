ad03a641cc2f719928c2f0bf471031481743dacb
iamrizwan077 <77151292+iamrizwan077@users.noreply.github.com>
2023-09-01 03:48:11 +0000

    Setting environment and files

---
+++ socialvibes/socialvibes_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SocialvibesAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "socialvibes_app"
---
+++ socialvibes/socialvibes_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+
+
+post(user, content, like, comment)
+user(firstname, lastname,email, password1, password2 )
+profile(bio, , phone no, picture)
